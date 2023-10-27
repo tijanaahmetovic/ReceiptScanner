546e2db96f660164754938d623985ece7081077b
tijanaahmetovic <48787837+tijanaahmetovic@users.noreply.github.com>
2023-10-27 15:18:36 +0200

    Add project files.

---
+++ ReceiptScanner/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ReceiptScanner
+{
+class Program
+    {
+        static HttpClient client = new HttpClient();
+        
+        static void ShowProduct(Product product)
+        {
+            Console.WriteLine($"Name: {product.Name}\tPrice: " +
+                $"{product.Price}\"Description: {product.Description}");
+        }
+        static async Task<List<Product>> GetProductAsync(string path)
+        {
+            List<Product> receipt = null;
+            HttpResponseMessage response = await client.GetAsync(path);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+                receipt = JsonConvert.DeserializeObject<List<Product>>(jsonString);
+            }
+            return receipt;
+        }
+
+
+        static void Main(string[] args)
+        {
+            RunAsync().GetAwaiter().GetResult();
+        }
+        static async Task RunAsync()
+        {
+            client.BaseAddress = new Uri("https://interview-task-api.mca.dev/qr-scanner-codes/alpha-qr-gFpwhsQ8fkY1");
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+
+
+            try
+            {
+                List<Product> products = await GetProductAsync("https://interview-task-api.mca.dev/qr-scanner-codes/alpha-qr-gFpwhsQ8fkY1");
+                Receipt receipt = new Receipt(products);
+                receipt.PrintReceipt();
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ReceiptScanner/Receipt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReceiptScanner
+{
+    class Receipt
+    {
+        public Receipt(List<Product> products)
+        {
+            Products = products;
+        }
+
+        public List<Product> Products { get; set; }
+
+        public void PrintReceipt()
+        {
+            var groups = this.Products.GroupBy(p => p.Domestic).Select(group =>
+                        new {
+                            Domestic = group.Key,
+                            Products = group.OrderBy(x => x.Name)
+                        });
+            foreach (var group in groups)
+            {
+                string domestic = group.Domestic ? "Domestic" : "Imported";
+                Console.WriteLine("."+ domestic);
+                foreach (var p in group.Products)
+                {
+                    Console.WriteLine(p.GetProductString());
+                }
+            }
+            Console.WriteLine(String.Format("Domestic cost: ${0:0.00}",this.DomesticCost(true)));
+            Console.WriteLine(String.Format("Imported cost: ${0:0.00}", this.DomesticCost(false)));
+            Console.WriteLine("Domestic count: " + this.DomesticCount(true));
+            Console.WriteLine("Imported count: " + this.DomesticCount(false));
+
+        }
+        public double DomesticCost(bool domestic)
+        {
+            return this.Products.Where(p => p.Domestic == domestic).Sum(p => p.Price);
+        }
+        public int DomesticCount(bool domestic)
+        {
+            return this.Products.Where(p => p.Domestic == domestic).Count();
+        }
+    }
+}
