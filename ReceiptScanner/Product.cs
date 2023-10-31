c6215570a5840d9548872104f186cbe951f55438
tijanaahmetovic <48787837+tijanaahmetovic@users.noreply.github.com>
2023-10-31 22:10:08 +0100

    Code fixes

--- ReceiptScanner/Product.cs
+++ ReceiptScanner/Product.cs
@@ -1,33 +1,36 @@
-﻿using Newtonsoft.Json;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace ReceiptScanner
 {
     class Product
     {
-        [JsonProperty("name")]
+        [JsonPropertyName("name"), JsonRequired]
         public string Name { get; set; }
 
-        [JsonProperty("domestic")]
+        [JsonPropertyName("domestic"), JsonRequired]
         public bool Domestic { get; set; }
 
-        [JsonProperty("price")]
+        [JsonPropertyName("price"), JsonRequired]
         public double Price { get; set; }
 
-        [JsonProperty("weight")]
-        public double Weight { get; set; }
+        [JsonPropertyName("weight")]
+        public double? Weight { get; set; }
 
-        [JsonProperty("description")]
+        [JsonPropertyName("description"), JsonRequired]
         public string Description { get; set; }
 
         public string GetProductString()
         {
+
             string WeightFormat = Weight > 0 ? Weight.ToString()+"g" : "N/A"; 
             return String.Format("...{0}\nPrice: ${1:0.00}\n{2}...\nWeight: {3}", Name, Price, Description.Substring(0, 10), WeightFormat);
+
         }
     }
 }
---
+++ ReceiptScanner/ReceiptRepo.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ReceiptScanner
+{
+    class ReceiptRepo
+    {
+
+        static string path = "https://mocki.io/v1/29cf0412-8229-4d35-a61a-c64939b946f8 ";
+
+        private HttpClient client;
+
+        public async Task<List<Product>> GetProductAsync()
+        {
+
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+            List<Product> products = null;
+            try
+            {
+                HttpResponseMessage response = await client.GetAsync(path);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonString = await response.Content.ReadAsStringAsync();
+                    products = JsonSerializer.Deserialize<List<Product>>(jsonString);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw new Exception();
+            }
+
+            return products;
+
+        }
+    }
+}
